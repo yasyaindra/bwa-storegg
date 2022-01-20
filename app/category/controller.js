821471a83be8b35b3a2899f2142df8ec5590bb43
yasyaindra <indrayasya96@gmail.com>
2022-01-20 11:27:23 +0800

    c in crud stands for create!

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,3 +1,5 @@
+const Category = require("./model");
+
 module.exports = {
   index: async (req, res) => {
     try {
@@ -10,6 +12,17 @@ module.exports = {
     try {
       res.render("admin/category/create");
     } catch (error) {
+      console.log(error.message);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+      let category = await Category({ name });
+      await category.save();
+
+      res.redirect("/category");
+    } catch {
       console.log(error.message);
     }
   },
