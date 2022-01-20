3e63f1a4c717a9deb7980606bda75c8f90207080
yasyaindra <indrayasya96@gmail.com>
2022-01-20 10:26:58 +0800

    category page

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,7 +1,9 @@
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("admin/category/view_category");
+      res.render("index", {
+        title: "Welcome",
+      });
     } catch (error) {
       console.log(error.message);
     }
