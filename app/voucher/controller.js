ff8e8291d10576d9d1c63c468ded05c12009b165
yasyaindra <indrayasya96@gmail.com>
2022-01-28 09:27:38 +0800

    :fire: done action status to change

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -181,4 +181,17 @@ module.exports = {
       res.redirect("/voucher");
     }
   },
+  actionStatus: async (req, res) => {
+    try {
+      const { id } = req.params;
+      let voucher = await Voucher.findOne({ _id: id });
+      let status = voucher.status === "Y" ? "N" : "Y";
+      voucher = await Voucher.findOneAndUpdate({ _id: id }, { status });
+      req.flash("alertMessage", "Berhasil ubah status");
+      req.flash("alertStatus", "success");
+      res.redirect("/voucher");
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
--- app/voucher/router.js
+++ app/voucher/router.js
@@ -7,6 +7,7 @@ const {
   viewEdit,
   actionEdit,
   actionDelete,
+  actionStatus,
 } = require("./controller");
 const multer = require("multer");
 const os = require("os");
@@ -26,5 +27,6 @@ router.post(
   multer({ dest: os.tmpdir() }).single("image"),
   actionCreate
 );
+router.put("/status/:id", actionStatus);
 
 module.exports = router;
