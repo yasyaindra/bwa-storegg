b940cfef7efafba2026524ac51013b25848247fa
yasyaindra <indrayasya96@gmail.com>
2022-01-31 09:51:29 +0800

    add timestamp

--- app/user/model.js
+++ app/user/model.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
 
-let userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Nama Harus Diisi"],
+let userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Nama Harus Diisi"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email harus diisi"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password harus diisi"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "admin",
+    },
+    status: {
+      type: String,
+      enum: ["Y", "N"],
+      default: "Y",
+    },
+    phoneNumber: {
+      type: String,
+      require: [true, "nomer telepon harus diisi"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email harus diisi"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password harus diisi"],
-  },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "admin",
-  },
-  status: {
-    type: String,
-    enum: ["Y", "N"],
-    default: "Y",
-  },
-  phoneNumber: {
-    type: String,
-    require: [true, "nomer telepon harus diisi"],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
