e03e831c83980eccf6419f6839220598f7e6f8e0
yasyaindra <indrayasya96@gmail.com>
2022-02-04 18:52:16 +0800

    :fire: done beranda

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,9 +1,25 @@
+const Transaction = require("../transaction/model");
+const Player = require("../user/model");
+const Voucher = require("../voucher/model");
+const Category = require("../category/model");
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("index", {
+      const transaction = await Transaction.countDocuments();
+      const player = await Player.countDocuments();
+      const voucher = await Voucher.countDocuments();
+      const category = await Category.countDocuments();
+
+      res.render("admin/dashboard/view_dashboard", {
         name: req.session.user.name,
         title: "Halaman Dashboard",
+        count: {
+          transaction,
+          player,
+          voucher,
+          category,
+        },
       });
     } catch (error) {
       console.log(error.message);
--- app/transaction/controller.js
+++ app/transaction/controller.js
@@ -7,7 +7,7 @@ module.exports = {
       const alertStatus = req.flash("alertStatus");
 
       const alert = { message: alertMessage, status: alertStatus };
-      const transaction = await Transaction.find().populate("player");
+      const transaction = await Transaction.find().populate("players");
 
       res.render("admin/transaction/view_transaction", {
         transaction,
@@ -15,6 +15,20 @@ module.exports = {
         name: req.session.user.name,
         title: "Halaman metode pembayaran",
       });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/transaction");
+    }
+  },
+  actionStatus: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { status } = req.query;
+      await Transaction.findByIdAndUpdate({ _id: id }, { status });
+      req.flash("alertMessage", `Berhasil ubah status`);
+      req.flash("alertStatus", "success");
+      res.redirect("/transaction");
     } catch (err) {
       req.flash("alertMessage", `${err.message}`);
       req.flash("alertStatus", "danger");
