e49e8cff325e4b96d5b15e4848c99ce03c51c453
yasyaindra <indrayasya96@gmail.com>
2022-02-13 13:26:42 -1200

    :fire: add history detail

--- app/player/controller.js
+++ app/player/controller.js
@@ -123,4 +123,66 @@ module.exports = {
       res.status(500).json({ message: err.message || `Internal server error` });
     }
   },
+  history: async (req, res) => {
+    try {
+      const { status = "" } = req.query;
+
+      let criteria = {};
+
+      if (status.length) {
+        criteria = {
+          ...criteria,
+          status: { $regex: `${status}`, $options: "i" },
+        };
+      }
+
+      if (req.player._id) {
+        criteria = {
+          ...criteria,
+          player: req.player._id,
+        };
+      }
+
+      console.log("=======================");
+      console.log(criteria);
+      console.log("=======================");
+
+      const history = await Transaction.find(criteria);
+
+      console.log("========HISTORY========");
+      console.log(history);
+      console.log("========HISTORY========");
+
+      let total = await Transaction.aggregate([
+        { $match: criteria },
+        {
+          $group: {
+            _id: null,
+            value: { $sum: "$value" },
+          },
+        },
+      ]);
+
+      console.log(total);
+
+      res.status(200).json({
+        data: history,
+        total: total.length ? total[0].value : 0,
+      });
+    } catch (error) {
+      res.status(404).json({ error: "Internal Error" });
+    }
+  },
+  historyDetail: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const history = await Transaction.findOne({ _id: id });
+      if (!history)
+        return res.status(404).json({ message: "Transaction is not found!" });
+
+      res.status(201).json({ data: history });
+    } catch (error) {
+      res.send(404).json({ message: "Internal error" });
+    }
+  },
 };
--- app/player/router.js
+++ app/player/router.js
@@ -1,11 +1,20 @@
 var express = require("express");
 var router = express.Router();
-const { landingPage, detailPage, category, checkout } = require("./controller");
+const {
+  landingPage,
+  detailPage,
+  category,
+  checkout,
+  history,
+  historyDetail,
+} = require("./controller");
 const { isLoginPlayer } = require("../middleware/auth");
 
 router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
+router.post("/history", isLoginPlayer, history);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
 
 module.exports = router;
